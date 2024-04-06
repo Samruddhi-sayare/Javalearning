232bdc03a5b55222bf10f050604c813a6693c59c
samru <samruddhisayre45@gmail.com>
2024-04-06 09:53:09 +0530

    initial commit

---
+++ src/main/java/Example_11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Example_11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        int num = (int)(43.54);
+//        System.out.println(num);
+        int number = 'a';
+        System.out.println(number);
+    }
+}
--- src/main/java/Example_9.java
+++ src/main/java/Example_9.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Example_9 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("enter the no.");
-        int rollno = input.nextInt();
-        System.out.println("your roll no. is"+rollno);
-
+//        System.out.println("enter the no. ");
+//        int rollno = input.nextInt();
+//        System.out.println("your roll no. is"+ rollno);
+String name = input.next();
+        System.out.println(name);
     }
 }
