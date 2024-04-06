5e560f9e33c1e50127b4bfb62f8e75f1692acae5
samru <samruddhisayre45@gmail.com>
2024-04-06 09:25:05 +0530

    initial commit

---
+++ src/main/java/Example_9.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Example_9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the no.");
+        int rollno = input.nextInt();
+        System.out.println("your roll no. is"+rollno);
+
+    }
+}
