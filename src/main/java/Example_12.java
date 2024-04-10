7da5aa4203f5cd1239451bb9c41cc04db8f1349d
samru <samruddhisayre45@gmail.com>
2024-04-10 08:25:10 +0530

    initial commit

---
+++ src/main/java/Example_12.java
@@ -0,0 +1,14 @@
+
+
+public class Example_12 {
+    public static void main(String[] args) {
+       int salary = 32400;
+       if (salary >2999){
+           salary = salary + 2000;
+       }
+       else{
+           salary = salary + 1000;
+       }
+        System.out.println(salary);
+    }
+}
