d150808042eca97ece9e8ba5d9a7410ce3b68e7e
samru <samruddhisayre45@gmail.com>
2024-05-11 12:07:07 +0530

    initial commit

---
+++ src/main/java/Example_22.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Example_22 {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int m = sc.nextInt();
+//    for (int n = 1 ; n < m ; n++ ){
+//        System.out.println(n);
+//    }
+//        int num = 1;
+//        while (num < 99){
+//            System.out.println(num);
+//            num++;
+//        }
+
+    }
+}
