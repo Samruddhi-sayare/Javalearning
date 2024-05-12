b102ee12c0709c641802b10dd001ac67f0e39c29
samru <samruddhisayre45@gmail.com>
2024-05-12 07:02:27 +0530

    initial commit

--- src/main/java/Example_29.java
+++ src/main/java/Example_29.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 public class Example_29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String fruits = sc.next();
+
+         switch (fruits){
+             case "mango":
+                 System.out.println("A king of fruits");
+                 break;
+             case " apple":
+                 System.out.println("red fruit");
+                 break;
+             case "orange":
+                 System.out.println("round");
+                 break;
+             default:
+                 System.out.println("please enter a valid fruit");
+         }
 
     }
 }
